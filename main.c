14b79471f25d81f29e96487a000e6b9b71a580ea
dammuozz <stefano.damaggio@gmail.com>
2016-08-23 17:03:27 +0200

    removed non I2C functions, adding LIS2HH12 register addresses

--- main.c
+++ main.c
@@ -21,12 +21,18 @@
  */
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include "boards.h"
 #include "app_util_platform.h"
 #include "app_uart.h"
 #include "app_error.h"
 #include "nrf_drv_twi.h"
 #include "nrf_delay.h"
+#include "nrf_gpio.h"
+
+const uint8_t leds_list[LEDS_NUMBER] = LEDS_LIST;
+
 
 /*Pins to connect shield. */
 #define ARDUINO_I2C_SCL_PIN 7
@@ -36,20 +42,50 @@
 #define UART_TX_BUF_SIZE 256
 #define UART_RX_BUF_SIZE 1
 
-/*Common addresses definition for accelereomter. */
-#define MMA7660_ADDR        (0x98U >> 1)
+/*Common addresses definition for accelereometer LIS2HH12. */
+#define LIS2HH12_ADDR				0b0011101	// 7bit I2C address
 
-#define MMA7660_REG_XOUT    0x00U
-#define MMA7660_REG_YOUT    0x01U
-#define MMA7660_REG_ZOUT    0x02U
-#define MMA7660_REG_TILT    0x03U
-#define MMA7660_REG_SRST    0x04U
-#define MMA7660_REG_SPCNT   0x05U
-#define MMA7660_REG_INTSU   0x06U
-#define MMA7660_REG_MODE    0x07U
-#define MMA7660_REG_SR      0x08U
-#define MMA7660_REG_PDET    0x09U
-#define MMA7660_REG_PD      0x0AU
+#define LIS2HH12_REG_TEMP_L			0x0B
+#define LIS2HH12_REG_TEMP_H			0x0C
+#define LIS2HH12_REG_WHO_AM_I		0x0F
+#define LIS2HH12_REG_ACT_THS		0x1E
+#define LIS2HH12_REG_ACT_DUR		0x1F
+#define LIS2HH12_REG_CTRL1			0x20
+#define LIS2HH12_REG_CTRL2			0x21
+#define LIS2HH12_REG_CTRL3			0x22
+#define LIS2HH12_REG_CTRL4			0x23
+#define LIS2HH12_REG_CTRL5			0x24
+#define LIS2HH12_REG_CTRL6			0x25
+#define LIS2HH12_REG_CTRL7			0x26
+#define LIS2HH12_REG_STATUS			0x27
+#define LIS2HH12_REG_OUT_X_L		0x28
+#define LIS2HH12_REG_OUT_X_H		0x29
+#define LIS2HH12_REG_OUT_Y_L		0x2A
+#define LIS2HH12_REG_OUT_Y_H		0x2B
+#define LIS2HH12_REG_OUT_Z_L		0x2C
+#define LIS2HH12_REG_OUT_Z_H		0x2D
+#define LIS2HH12_REG_FIFO_CTRL		0x2E
+#define LIS2HH12_REG_FIFO_SRC		0x2F
+#define LIS2HH12_REG_IG_CFG1		0x30
+#define LIS2HH12_REG_IG_SRC1		0x31
+#define LIS2HH12_REG_IG_THS_X1		0x32
+#define LIS2HH12_REG_IG_THS_Y1		0x33
+#define LIS2HH12_REG_IG_THS_Z1		0x34
+#define LIS2HH12_REG_IG_DUR1		0x35
+#define LIS2HH12_REG_IG_CFG2		0x36
+#define LIS2HH12_REG_SRC2			0x37
+#define LIS2HH12_REG_IG_THS2		0x38
+#define LIS2HH12_REG_IG_DUR2		0x39
+#define LIS2HH12_REG_XL_REFERENCE	0x3A
+#define LIS2HH12_REG_XH_REFERENCE	0x3B
+#define LIS2HH12_REG_YL_REFERENCE	0x3C
+#define LIS2HH12_REG_YH_REFERENCE	0x3D
+#define LIS2HH12_REG_ZL_REFERENCE	0x3E
+#define LIS2HH12_REG_ZH_REFERENCE	0x3F
+
+#define LIS2HH12_REG_FIFO_stream		01001000b	// to set fifo in Stream mode, just for testing
+
+
 
 /* Mode for MMA7660. */
 #define ACTIVE_MODE 1u
@@ -79,7 +115,7 @@ typedef struct
     int16_t y;
     int16_t z;
 } sum_t;
-static sum_t m_sum = {0};
+//static sum_t m_sum = {0};
 
 /**
  * @brief Union to keep raw and converted data from accelerometer samples at one memory space.
@@ -117,7 +153,7 @@ typedef struct
 #endif
 #endif
 /* Buffer for samples. */
-static sample_t m_sample_buffer[NUMBER_OF_SAMPLES] = {0};
+//static sample_t m_sample_buffer[NUMBER_OF_SAMPLES] = {0};
 #ifdef __GNUC_PATCHLEVEL__
 #if GCC_VERSION < 50505
 #pragma GCC diagnostic pop
@@ -128,19 +164,9 @@ static volatile bool m_xfer_done = true;
 /* Indicates if setting mode operation has ended. */
 static volatile bool m_set_mode_done = false;
 /* TWI instance. */
-static const nrf_drv_twi_t m_twi_mma_7660 = NRF_DRV_TWI_INSTANCE(0);
+static const nrf_drv_twi_t m_twi_LIS2HH12 = NRF_DRV_TWI_INSTANCE(0);
 
-/**
- * @brief Function for casting 6 bit uint to 6 bit int.
- *
- */
-__STATIC_INLINE void int_to_uint(int8_t * put, uint8_t data)
-{
-    if (!(data & MMA7660_FAILURE_FLAG))     //6th bit is failure flag - we cannot read sample
-    {
-        *put = (int8_t)(data << 2) / 4;
-    }
-}
+
 
 /**
  * @brief UART events handler.
@@ -194,82 +220,34 @@ static void uart_config(void)
 /**
  * @brief Function for setting active mode on MMA7660 accelerometer.
  */
-void MMA7660_set_mode(void)
+/*void LIS2HH12_set_mode(void)
 {
     ret_code_t err_code;
-    /* Writing to MMA7660_REG_MODE "1" enables the accelerometer. */
+    // Writing to MMA7660_REG_MODE "1" enables the accelerometer.
     uint8_t reg[2] = {MMA7660_REG_MODE, ACTIVE_MODE};
 
-    err_code = nrf_drv_twi_tx(&m_twi_mma_7660, MMA7660_ADDR, reg, sizeof(reg), false);  
+    err_code = nrf_drv_twi_tx(&m_twi_LIS2HH12, LIS2HH12_ADDR, reg, sizeof(reg), false);
     APP_ERROR_CHECK(err_code);
     
     while(m_set_mode_done == false);
-}
+}*/
 
 /**
- * @brief Function for averaging samples from accelerometer.
+ * @brief Function for checking the WHO_AM_I register to test I2C connection
  */
-void read_data(sample_t * p_new_sample)
+void LIS2HH12_I2Ctest(void)
 {
-    /* Variable to count samples. */
-    static uint8_t sample_idx;
-    static uint8_t prev_tilt;
-    
-    sample_t * p_sample = &m_sample_buffer[sample_idx];
-    
-    /* Subtracting oldest sample. */
-    m_sum.x    -= p_sample->x.conv;
-    m_sum.y    -= p_sample->y.conv;
-    m_sum.z    -= p_sample->z.conv;
-    
-    p_sample->tilt = p_new_sample->tilt;    
-    
-    int_to_uint(&p_sample->x.conv, p_new_sample->x.raw);
-    int_to_uint(&p_sample->y.conv, p_new_sample->y.raw);
-    int_to_uint(&p_sample->z.conv, p_new_sample->z.raw);
-    
-    /* Adding new sample. This way we always have defined number of samples. */
-    m_sum.x    += p_sample->x.conv;
-    m_sum.y    += p_sample->y.conv;
-    m_sum.z    += p_sample->z.conv;
+    /*ret_code_t err_code;
 
-    ++sample_idx;
-    if (sample_idx >= NUMBER_OF_SAMPLES)
-    {
-        sample_idx = 0;
-    }
+    uint8_t reg[2] = {MMA7660_REG_MODE, ACTIVE_MODE};
 
-    if (sample_idx == 0 || (prev_tilt && (prev_tilt != p_sample->tilt)))
-    {
-        char const * orientation;
-        switch ((p_sample->tilt >> 2) & 0x07)
-        {
-            case LEFT: 
-                orientation = "LEFT";
-                break;
-            case RIGHT:
-                orientation = "RIGHT"; 
-                break;
-            case DOWN:             
-                orientation = "DOWN";  
-                break;
-            case UP:
-                orientation = "UP";    
-                break;
-            default: 
-                orientation = "?";     
-                break;
-        }
-        printf("X: %3d, Y: %3d, Z: %3d | %s%s%s\r\n",
-                m_sum.x / NUMBER_OF_SAMPLES,
-                m_sum.y / NUMBER_OF_SAMPLES,
-                m_sum.z / NUMBER_OF_SAMPLES,
-                orientation,
-                (p_sample->tilt & TILT_TAP_MASK) ? " TAP"   : "",
-                (p_sample->tilt & TILT_SHAKE_MASK) ? " SHAKE" : "");
-                prev_tilt = p_sample->tilt;
-    }
+    err_code = nrf_drv_twi_tx(&m_twi_LIS2HH12, LIS2HH12_ADDR, LIS2HH12_REG_WHO_AM_I, sizeof(LIS2HH12_REG_WHO_AM_I), false);
+    APP_ERROR_CHECK(err_code);
+
+    while(m_set_mode_done == false);
+    */
 }
+
 
 /**
  * @brief TWI events handler.
@@ -279,11 +257,12 @@ void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
     ret_code_t err_code;
     static sample_t m_sample;
     
+    printf("\n\revent generated on I2C!\r\n");
+
     switch(p_event->type)
     {
         case NRF_DRV_TWI_EVT_DONE:
-            if ((p_event->type == NRF_DRV_TWI_EVT_DONE) &&
-                (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_TX))
+            if ((p_event->type == NRF_DRV_TWI_EVT_DONE) && (p_event->xfer_desc.type == NRF_DRV_TWI_XFER_TX))
             {
                 if(m_set_mode_done != true)
                 {
@@ -292,12 +271,12 @@ void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
                 }
                 m_xfer_done = false;
                 /* Read 4 bytes from the specified address. */
-                err_code = nrf_drv_twi_rx(&m_twi_mma_7660, MMA7660_ADDR, (uint8_t*)&m_sample, sizeof(m_sample));
+                err_code = nrf_drv_twi_rx(&m_twi_LIS2HH12, LIS2HH12_ADDR, (uint8_t*)&m_sample, sizeof(m_sample));
                 APP_ERROR_CHECK(err_code);
             }
             else
             {
-                read_data(&m_sample);
+                //read_data(&m_sample);
                 m_xfer_done = true;
             }
             break;
@@ -313,17 +292,17 @@ void twi_init (void)
 {
     ret_code_t err_code;
     
-    const nrf_drv_twi_config_t twi_mma_7660_config = {
+    const nrf_drv_twi_config_t twi_LIS2HH12_config = {
        .scl                = ARDUINO_SCL_PIN,
        .sda                = ARDUINO_SDA_PIN,
        .frequency          = NRF_TWI_FREQ_100K,
        .interrupt_priority = APP_IRQ_PRIORITY_HIGH
     };
     
-    err_code = nrf_drv_twi_init(&m_twi_mma_7660, &twi_mma_7660_config, twi_handler, NULL);
+    err_code = nrf_drv_twi_init(&m_twi_LIS2HH12, &twi_LIS2HH12_config, twi_handler, NULL);
     APP_ERROR_CHECK(err_code);
     
-    nrf_drv_twi_enable(&m_twi_mma_7660);
+    nrf_drv_twi_enable(&m_twi_LIS2HH12);
 }
 
 /**
@@ -331,24 +310,28 @@ void twi_init (void)
  */
 int main(void)
 {
+	// Configure LED-pins as outputs for debugging.
+	LEDS_CONFIGURE(LEDS_MASK);
+
     uart_config();
-   // int a = __GNUC__, c = __GNUC_PATCHLEVEL__;
-    printf("\n\rTWI sensor example\r\n");
+    // int a = __GNUC__, c = __GNUC_PATCHLEVEL__;
+    printf("\n\rLIS2HH12 sensor example\r\n");
     twi_init();
-    MMA7660_set_mode(); 
+    //LIS2HH12_set_mode();
     
     uint8_t reg = 0;
     ret_code_t err_code;
     
+
     while(true)
     {
         nrf_delay_ms(100);
-        /* Start transaction with a slave with the specified address. */
+        // Start transaction with a slave with the specified address.
         do
         {
             __WFE();
         }while(m_xfer_done == false);
-        err_code = nrf_drv_twi_tx(&m_twi_mma_7660, MMA7660_ADDR, &reg, sizeof(reg), true);
+        err_code = nrf_drv_twi_tx(&m_twi_LIS2HH12, LIS2HH12_ADDR, &reg, sizeof(reg), true);
         APP_ERROR_CHECK(err_code);
         m_xfer_done = false;
     }
