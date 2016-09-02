ddd7fab4118a7221c1b5085b9db8387baa31665f
dammuozz <stefano.damaggio@gmail.com>
2016-09-02 17:48:42 +0200

    moving to app_twi transactions, nothing works, app_twi_perform returns error 16

--- main.c
+++ main.c
@@ -24,16 +24,16 @@
 #include <stdint.h>
 #include <float.h>
 #include <math.h>
-#include "boards.h"
 #include "app_util_platform.h"
 #include "app_error.h"
+#include "app_twi.h"
+#include "boards.h"
 #include "nrf_drv_twi.h"
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
 #include "nrf_log.h"
+#include "nrf_error.h"
 #include "lis2.h"
-
-const uint8_t leds_list[LEDS_NUMBER] = LEDS_LIST;
 
 
 /*Pins to connect shield. */
@@ -84,205 +84,105 @@ const uint8_t leds_list[LEDS_NUMBER] = LEDS_LIST;
 #define	_LIS2_ODR_MASK			0x8F
 #define	_LIS2_ODR_SHIFT			4
 
+#define MAX_PENDING_TWI_TRANSACTIONS    4
+#define READ_BUFFER_SIZE    			5
+#define _LIS2_TEST_TRANSFERS			1
+
 int16_t lis2Accel[3];
 float lis2Tilt;
+uint8_t readBuffer[READ_BUFFER_SIZE];
+
+uint8_t err_code_test;
+static uint8_t const buf[] = {_LIS2_REG_CTRL1, 0};
+app_twi_transfer_t const transfers[_LIS2_TEST_TRANSFERS] =
+{
+	APP_TWI_WRITE(_LIS2_I2CADDR, buf, sizeof(buf), 0),
+};
 
 /* TWI instance. */
-static const nrf_drv_twi_t m_twi_LIS2 = NRF_DRV_TWI_INSTANCE(0);
+static app_twi_t m_app_twi = APP_TWI_INSTANCE(0);
 
-/**
- * @brief I2C initialization.
- */
-static void twi_init()
+static void twi_config(void)
 {
-	ret_code_t err_code;
+    uint32_t err_code;
 
-	const nrf_drv_twi_config_t twi_LIS2_config = { .scl =
-			ARDUINO_I2C_SCL_PIN, .sda = ARDUINO_I2C_SDA_PIN, .frequency =
-			NRF_TWI_FREQ_100K, .interrupt_priority = APP_IRQ_PRIORITY_HIGH };
+    nrf_drv_twi_config_t const config = {
+       .scl                = ARDUINO_SCL_PIN,
+       .sda                = ARDUINO_SDA_PIN,
+       .frequency          = NRF_TWI_FREQ_100K,
+       .interrupt_priority = APP_IRQ_PRIORITY_LOW
+    };
 
-	err_code = nrf_drv_twi_init(&m_twi_LIS2, &twi_LIS2_config, NULL, NULL);
-	APP_ERROR_CHECK(err_code);
-
-	nrf_drv_twi_enable(&m_twi_LIS2);
+    APP_TWI_INIT(&m_app_twi, &config, MAX_PENDING_TWI_TRANSACTIONS, err_code);
+    APP_ERROR_CHECK(err_code);
 }
 
-/**
- * @brief Function to initialize the lis2 accelerometer
- *
- * Default initialization include +/-2g sensitivity, odr set to LIS2_POWERDOWN,
- * FIFO set to Bypass mode and interrupts disabled
- */
 void lis2Init()
 {
-	twi_init();
+	twi_config();
 }
 
-/**
- * @brief Function to read lis2 accelerometer register
- *
- * @param regAddr register address to be read
- */
-static int _lis2ReadReg(uint8_t regAddr)
+//static void _lis2ReadRegTran()
+//{
+//	uint8_t regAddr = _LIS2_REG_WHO_AM_I;
+//	//uint8_t res = 0;
+//	uint8_t err_code;
+//
+//	//static uint8_t const buf[] = { LM75B_REG_CONF, 0 };
+//
+//    app_twi_transfer_t const transfers[] =
+//    {
+//		APP_TWI_WRITE(_LIS2_I2CADDR, &regAddr, sizeof(regAddr), APP_TWI_NO_STOP),
+//		APP_TWI_READ (_LIS2_I2CADDR, readBuffer, READ_BUFFER_SIZE, 0)
+//    };
+//
+////    uint8_t const lm75b_conf_reg_addr  = LM75B_REG_CONF;
+////    LM75B_READ(&lm75b_conf_reg_addr,  &m_buffer[0], 1),
+////	#define LM75B_READ(p_reg_addr, p_buffer, byte_cnt) APP_TWI_WRITE(LM75B_ADDR, p_reg_addr, 1, APP_TWI_NO_STOP), APP_TWI_READ (LM75B_ADDR, p_buffer, byte_cnt, 0)
+//
+//    NRF_LOG_PRINTF("app_twi_perform -pre \r\n");
+//    nrf_delay_ms(200);
+//    // Blocking mode: When an application schedules a transaction in blocking mode (app_twi_perform),
+//    // the function does not return until the transaction has finished (successfully or with an error).
+//    err_code = app_twi_perform(&m_app_twi, transfers, 2, NULL);
+//    NRF_LOG_PRINTF("app_twi_perform %d \r\n", err_code);
+//    nrf_delay_ms(500);
+//    APP_ERROR_CHECK(err_code);
+//}
+
+static void _lis2WriteRegTran()
 {
-	uint8_t res = 0;
-	ret_code_t errCode;
-
-	errCode = nrf_drv_twi_tx(&m_twi_LIS2, _LIS2_I2CADDR, &regAddr, sizeof(regAddr), true);
-	APP_ERROR_CHECK(errCode);
-
-	errCode = nrf_drv_twi_rx(&m_twi_LIS2, _LIS2_I2CADDR, (uint8_t*)&res, sizeof(res));
-	APP_ERROR_CHECK(errCode);
-	//NRF_LOG_PRINTF("\n\rnrf_drv_twi_rx result:%02x err_code: %d\r\n", res, err_code);
-
-	return res;
-}
-
-/**
- * @brief Function to write lis2 accelerometer register
- *
- * @param regAddr	address of the register to be written
- * @param regValue 	register value to be written
- */
-static void _lis2WriteReg(uint8_t regAddr, uint8_t regValue)
-{
-	APP_ERROR_CHECK(nrf_drv_twi_tx(&m_twi_LIS2, _LIS2_I2CADDR, (uint8_t[]){regAddr, regValue}, sizeof((uint8_t[]){regAddr, regValue}), true));
-}
-
-/**
- * @brief Function to set lis2 output data rate
- *
- * Set by default to LIS2_POWERDOWN.
- * The higher the frequency the higher the power consumption.
- * 
- * @param	odr selected Output Data Rate from available odr in enum Lis2OutputDataRate
- * @return	true if successful, false otherwise
- */
-bool lis2SetOutputDataRate(Lis2OutputDataRate odr)
-{
-	static uint8_t regCtrl1 = -1;
-	static uint8_t odrValue = 0;
-
-	regCtrl1 = _lis2ReadReg(_LIS2_REG_CTRL1);
-
-	if(regCtrl1 == -1) {
-		NRF_LOG_PRINTF("set_odr(): LIS2HH12_REG_CTRL1 read unsuccessful!");
-		return false;
-	}
-
-	switch(odr) {
-		case LIS2_POWERDOWN: odrValue = 0;
-			break;
-		case LIS2_ODR10HZ: odrValue = 1;
-			break;
-		case LIS2_ODR50HZ: odrValue = 2;
-			break;
-		case LIS2_ODR100HZ: odrValue = 3;
-			break;
-		case LIS2_ODR200HZ: odrValue = 4;
-			break;
-		case LIS2_ODR400HZ: odrValue = 5;
-		  	break;
-		case LIS2_ODR800HZ: odrValue = 6;
-		  	break;
-	    default: NRF_LOG_PRINTF("lis2SetOutputDataRate: Invalid odr value\n"); return false;
-	    	break;
-	}
-
-	_lis2WriteReg(_LIS2_REG_CTRL1, (regCtrl1 & _LIS2_ODR_MASK) | (odrValue << _LIS2_ODR_SHIFT));
-
-	return true;
-}
-
-
-/**
- * @brief Function to update lis2 temperature reading
- *
- * WARNING: value seems to not make sense!
- * Temperature value is stored in 2 8bit registers, 16 bit total
- * Resolution is 11 bit expressed in two's complement
- * Range is from -40 to +85 degC so 125 total, 0 is at ((40+85)/2)+40=22.5degC
- * Ratio is then 0.061035degC/bit
- * Tout = (int16_t)value * ratio + 22.5degC
- *
- * @return	int16_t temperature register value
- */
-int16_t lis2GetTemp()
-{
-	int16_t temp_val = 0;
-
-	temp_val = (_lis2ReadReg(_LIS2_REG_TEMP_H) << 8 | _lis2ReadReg(_LIS2_REG_TEMP_L));
-	//NRF_LOG_PRINTF("temp register: %d\r\n", temp_val);
-
-	return temp_val;
-}
-
-/**
- * @brief Function to calculate lis2 tilt angle from acceleration readings
- *
- * @return tilt angle in degrees
- */
-static float _lis2GetTilt()
-{
-	/* tan(tilt) = sqrt(x^2+y^2)/z
-	 * tilt = atan( sqrt(x^2+y^2) / z )
-	 *
-	 * par1 = sqrt(x^2+y^2)
-	 */
-	float par1 = sqrtf(powf(lis2Accel[LIS2_X],2) + powf(lis2Accel[LIS2_Y],2));
-	float tilt = atan2f(par1, lis2Accel[LIS2_Z]);
-
-	// convert from radians to degrees
-	return tilt * (180.0f / (float)M_PI);
-}
-
-
-/**
- * @brief Function to update lis2 x, y and z acceleration readings
- */
-void lis2Update()
-{
-	// 8+8=16 bit registers contain a signed int value which min is -2g (-32768 or -2^15) and max is +2g (32767)
-	int16_t regX = (_lis2ReadReg(_LIS2_REG_OUT_X_H) << 8 | _lis2ReadReg(_LIS2_REG_OUT_X_L));
-	int16_t regY = (_lis2ReadReg(_LIS2_REG_OUT_Y_H) << 8 | _lis2ReadReg(_LIS2_REG_OUT_Y_L));
-	int16_t regZ = (_lis2ReadReg(_LIS2_REG_OUT_Z_H) << 8 | _lis2ReadReg(_LIS2_REG_OUT_Z_L));
-
-	/* input range is (-2^15, +2^15-1)
-	 * output range is (-2000mg, +1999mg)
-	 * ratio is 16,384 bit/g or (2^14/1000) bit/g
-	 *
-	 * multiply for 1/ratio instead of dividing
-	 */
-	int32_t xVal = (regX * 1000) >> 14;
-	int32_t yVal = (regY * 1000) >> 14;
-	int32_t zVal = (regZ * 1000) >> 14;
-
-	lis2Accel[LIS2_X] = (int16_t)xVal;
-	lis2Accel[LIS2_Y] = (int16_t)yVal;
-	lis2Accel[LIS2_Z] = (int16_t)zVal;
-
-	// update tilt angle
-	lis2Tilt = _lis2GetTilt();
+    // Blocking mode: When an application schedules a transaction in blocking mode (app_twi_perform),
+    // the function does not return until the transaction has finished (successfully or with an error).
+	err_code_test = app_twi_perform(&m_app_twi, transfers, _LIS2_TEST_TRANSFERS, NULL);
 }
 
 /**
  * @brief Function for main application entry.
  */
 int main(void) {
-
+	bool b = false;
+	NRF_LOG_PRINTF("\n\rLIS2HH12 accelerometer library demo\r\n");
 	lis2Init();
-	nrf_delay_ms(50);
-	lis2SetOutputDataRate(LIS2_ODR10HZ);
-	nrf_delay_ms(50);
+	nrf_delay_ms(1000);
+	//lis2SetOutputDataRate(LIS2_ODR10HZ);
+	//nrf_delay_ms(50);
 
 	while(true) {
-		NRF_LOG_PRINTF("\n\rLIS2HH12 accelerometer library demo\r\n");
+		b =! b;
+		NRF_LOG_PRINTF("__%d__\r\n", b);
+		_lis2WriteRegTran();
+		NRF_LOG_PRINTF("app_twi_perform() error:%d \r\n", err_code_test);
+		nrf_delay_ms(1000);
+		APP_ERROR_CHECK(err_code_test);
+
+		/*
 		lis2Update();
 		NRF_LOG_PRINTF("X acceleration:%d mG\r\n", lis2Accel[LIS2_X]);
 		NRF_LOG_PRINTF("Y acceleration:%d mG\r\n", lis2Accel[LIS2_Y]);
 		NRF_LOG_PRINTF("Z acceleration:%d mG\r\n", lis2Accel[LIS2_Z]);
 		printf("tilt: %f deg\r\n", lis2Tilt);
-
+		*/
 		/*
 		NRF_LOG_PRINTF("_LIS2_REG_CTRL1 %04x\r\n", _lis2ReadReg(_LIS2_REG_CTRL1));
 		NRF_LOG_PRINTF("_LIS2_REG_CTRL2 %04x\r\n", _lis2ReadReg(_LIS2_REG_CTRL2));
@@ -294,7 +194,7 @@ int main(void) {
 		*/
 
 		NRF_LOG_PRINTF("\r\n");
-		nrf_delay_ms(1000);
+		nrf_delay_ms(3000);
 	}
 }
 
